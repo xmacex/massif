999c1b4e727e00dc6959d49cf39425311d355d31
justmat <justmat@mykolab.com>
2020-08-05 21:33:49 +0700

    removes offset for now

--- lib/Engine_ResonatorBank.sc
+++ lib/Engine_ResonatorBank.sc
@@ -7,19 +7,19 @@ Engine_ResonatorBank : CroneEngine {
   }
 
   alloc {
-    SynthDef(\resonatorbank, {|inL, inR, out, amp=0.1, offset=0,
+    SynthDef(\resonatorbank, {|inL, inR, out, amp=0.1,
       freq1, freq2, freq3, freq4, freq5, freq6, freq7, freq8, 
       amp1=0.25, amp2=0.25, amp3=0.25, amp4=0.25, amp5=0.25, amp6=0.25, amp7=0.25, amp8=0.25,
       ring1=0.1, ring2=0.1, ring3=0.1, ring4=0.1, ring5=0.1, ring6=0.1, ring7=0.1, ring8=0.1|
 
-      var freqs = [freq1 + offset, freq2 + offset, freq3 + offset, freq4 + offset, freq5 + offset, freq6 + offset, freq7 + offset, freq8 + offset];
+      var freqs = [freq1, freq2, freq3, freq4, freq5, freq6 , freq7, freq8];
       var amps = [amp1, amp2, amp3, amp4, amp5, amp6, amp7, amp8];
       var rings = [ring1, ring2, ring3, ring4, ring5, ring6, ring7, ring8];
 
       var sound = [In.ar(inL), In.ar(inR)];
       var resonators = DynKlank.ar(`[freqs, amps, rings], sound);
 
-      Out.ar(out, resonators * amp).softclip;
+      Out.ar(out, resonators * amp);
     }).add;
 
     context.server.sync;
@@ -34,10 +34,6 @@ Engine_ResonatorBank : CroneEngine {
 // commands
     this.addCommand("amp", "f", {|msg|
       synth.set(\amp, msg[1]);
-    });
-    
-    this.addCommand("offset", "f", {|msg|
-      synth.set(\offset, msg[1]);
     });
 
     this.addCommand("freq1", "f", {|msg|
--- massif.lua
+++ massif.lua
@@ -10,96 +10,131 @@
 --
 
 engine.name = "ResonatorBank"
-local MusicUtil = require "musicutil"
+local MU = require "musicutil"
 
-local current_page = 1
+local current_res = 1
 
 local mode = 0 -- 0 = play, 1 = edit
 local alt = false
-local pitch_tune = false
+--local pitch_tune = false
 local midi_tune = false
 
 local dirty = true
+
+local scale_options = {}
+
+
+local function pitch_tune(freq)
+  -- set the freq of the currently selected resonator
+  -- only relevant in edit mode
+  params:set("freq" .. current_res, freq)
+end
+
+
+local function pitch_multi_tune(freq)
+  -- set the freqs of the entier bank
+  -- use musicutils to get the closest midi note
+  -- treat this as the root and tune the resonators to a scale
+  local note = MU.freq_to_note_num(freq)
+  local scale = MU.generate_scale_of_length(note, params:get("scale"), 32)
+  
+  for i = 1, 8 do
+    local n = math.random(1, #scale)
+    local f = MU.note_num_to_freq(scale[n])
+
+    params:set("freq" .. i, f)
+  end
+end
 
 
 function init()
   m = midi.connect()
   m.event = midi_event
+  
+  for _, v in ipairs(MU.SCALES) do
+    table.insert(scale_options, v.name)
+  end
   --params:add_control("amp", "amp", controlspec.new(0.0, 1.0, "lin", 0.01, .05))
   --params:set_action("amp", function(v) engine.amp(v) end)
-
-  params:add_control("freq1", "freq1", controlspec.new(20, 10000, "lin", 1, math.random(20, 10000), "hz"))
+  params:add_separator()
+    
+  params:add_group("tuning", 8)
+  params:add_control("freq1", "freq1", controlspec.new(20, 10000, "lin", 1, 50, "hz"))
   params:set_action("freq1", function(v) engine.freq1(v) end)
 
-  params:add_control("freq2", "freq2", controlspec.new(20, 10000, "lin", 1, math.random(20, 10000), "hz"))
+  params:add_control("freq2", "freq2", controlspec.new(20, 10000, "lin", 1, 110, "hz"))
   params:set_action("freq2", function(v) engine.freq2(v) end)
   
-  params:add_control("freq3", "freq3", controlspec.new(20, 10000, "lin", 1, math.random(200, 10000), "hz"))
+  params:add_control("freq3", "freq3", controlspec.new(20, 10000, "lin", 1, 220, "hz"))
   params:set_action("freq3", function(v) engine.freq3(v) end)
   
-  params:add_control("freq4", "freq4", controlspec.new(20, 10000, "lin", 1, math.random(300, 10000), "hz"))
+  params:add_control("freq4", "freq4", controlspec.new(20, 10000, "lin", 1, 500, "hz"))
   params:set_action("freq4", function(v) engine.freq4(v) end)
   
-  params:add_control("freq5", "freq5", controlspec.new(2000, 18000, "lin", 1, math.random(1000, 18000), "hz"))
+  params:add_control("freq5", "freq5", controlspec.new(20, 10000, "lin", 1, 1100, "hz"))
   params:set_action("freq5", function(v) engine.freq5(v) end)
   
-  params:add_control("freq6", "freq6", controlspec.new(2000, 18000, "lin", 1, math.random(2000, 18000), "hz"))
+  params:add_control("freq6", "freq6", controlspec.new(20, 10000, "lin", 1, 2200, "hz"))
   params:set_action("freq6", function(v) engine.freq6(v) end)
   
-  params:add_control("freq7", "freq7", controlspec.new(2000, 18000, "lin", 1, math.random(4000, 18000), "hz"))
+  params:add_control("freq7", "freq7", controlspec.new(20, 10000, "lin", 1, 5200, "hz"))
   params:set_action("freq7", function(v) engine.freq7(v) end)
   
-  params:add_control("freq8", "freq8", controlspec.new(2000, 18000, "lin", 1, math.random(8000, 18000), "hz"))
+  params:add_control("freq8", "freq8", controlspec.new(20, 10000, "lin", 1, 11000, "hz"))
   params:set_action("freq8", function(v) engine.freq8(v) end)
 
-  params:add_control("amp1", "amp1", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_group("amplitudes", 8)
+  params:add_control("amp1", "amp1", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp1", function(v) engine.amp1(v) end)
   
-  params:add_control("amp2", "amp2", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp2", "amp2", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp2", function(v) engine.amp2(v) end)
   
-  params:add_control("amp3", "amp3", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp3", "amp3", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp3", function(v) engine.amp3(v) end)
   
-  params:add_control("amp4", "amp4", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp4", "amp4", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp4", function(v) engine.amp4(v) end)
   
-  params:add_control("amp5", "amp5", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp5", "amp5", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp5", function(v) engine.amp5(v) end)
   
-  params:add_control("amp6", "amp6", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp6", "amp6", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp6", function(v) engine.amp6(v) end)
   
-  params:add_control("amp7", "amp7", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp7", "amp7", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp7", function(v) engine.amp7(v) end)
   
-  params:add_control("amp8", "amp8", controlspec.new(0, 1, "lin", 0.01, .2))
+  params:add_control("amp8", "amp8", controlspec.new(0, 1, "lin", 0.01, .15))
   params:set_action("amp8", function(v) engine.amp8(v) end)
-  
-  params:add_control("ring1", "ring1", controlspec.new(0, 5, "lin", 0.01, .10))
+
+  params:add_group("ring times", 8)
+  params:add_control("ring1", "ring1", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring1", function(v) engine.ring1(v) end) 
   
-  params:add_control("ring2", "ring2", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring2", "ring2", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring2", function(v) engine.ring2(v) end)
   
-  params:add_control("ring3", "ring3", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring3", "ring3", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring3", function(v) engine.ring3(v) end) 
   
-  params:add_control("ring4", "ring4", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring4", "ring4", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring4", function(v) engine.ring4(v) end) 
   
-  params:add_control("ring5", "ring5", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring5", "ring5", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring5", function(v) engine.ring5(v) end) 
   
-  params:add_control("ring6", "ring6", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring6", "ring6", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring6", function(v) engine.ring6(v) end) 
   
-  params:add_control("ring7", "ring7", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring7", "ring7", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring7", function(v) engine.ring7(v) end) 
   
-  params:add_control("ring8", "ring8", controlspec.new(0, 5, "lin", 0.01, .10))
+  params:add_control("ring8", "ring8", controlspec.new(0, 5, "lin", 0.01, .05))
   params:set_action("ring8", function(v) engine.ring8(v) end)
-  
+
+  params:add_separator("auto tune settings")
+  params:add_option("scale", "scale", scale_options, 1)
   params:bang()
   
   local screen_metro = metro.init()
@@ -110,8 +145,12 @@ function init()
   pitch_tracker = poll.set("pitch_in_l")
   pitch_tracker.callback = function(x)
     if x > 0 then
-      if pitch_tune  == true then
-        params:set("freq" .. current_page, x)
+
+      if mode == 1 then
+        --params:set("freq" .. current_page, x)
+        pitch_tune(x)
+      else
+        pitch_multi_tune(x)
       end
     end
   end
@@ -126,14 +165,20 @@ function key(n, z)
     elseif n == 3 and z == 1 then
       mode = 1
     end
-  elseif mode == 1 then
-    if n == 2 then 
-      pitch_tune = z == 1 and true or false 
-      pitch_tracker:update()
-    end
-    if n == 3 then midi_tune = z == 1 and true or false end
   else
-    
+    if mode == 1 then
+      if n == 2 and z == 1 then 
+       --pitch_tune = z == 1 and true or false 
+        pitch_tracker:update()
+      elseif n == 3 and z == 1 then
+        midi_tune = z == 1 and true or false
+      end
+    elseif mode == 0 then
+      if n == 2 and z == 1 then
+        --pitch_tune = z == 1 and true or false 
+        pitch_tracker:update()
+      end
+    end
   end
   dirty = true
 end
@@ -142,16 +187,24 @@ end
 function enc(n, d)
   if mode == 1 and alt then
     if n == 1 then
-      current_page = util.clamp(current_page + d, 1, 8)
+      current_res = util.clamp(current_res + d, 1, 8)
     end
-  else
+  elseif mode == 1 then
     if n == 1 then
-      params:delta("freq" .. current_page, d)
+      params:delta("freq" .. current_res, d)
     elseif n == 2 then
-      params:delta("amp" .. current_page, d)
+      params:delta("amp" .. current_res, d)
     elseif n == 3 then
-      params:delta("ring" .. current_page, d)
+      params:delta("ring" .. current_res, d)
     end
+  elseif mode == 0 then
+    --[[if n == 1 then
+      params:delta("freqs_offset", d)
+    elseif n == 2 then
+      params:delta("rings_offset", d)
+    elseif n == 3 then
+      params:delta("amps_offset", d)
+    end]]
   end
   dirty = true
 end
@@ -163,7 +216,7 @@ local function draw_edit()
   screen.move(64, 16)
   screen.font_size(24)
   screen.font_face(1)
-  screen.text_center(current_page)
+  screen.text_center(current_res)
   screen.move(64, 22)
   screen.text_center(".................")
   screen.font_size(13)
@@ -171,15 +224,15 @@ local function draw_edit()
   screen.move(15, 36)
   screen.text("freq: ")
   screen.move(115, 36)
-  screen.text_right(params:get("freq" .. current_page) .. " hz")
+  screen.text_right(params:get("freq" .. current_res) .. " hz")
   screen.move(15, 48)
   screen.text("amp: ")
   screen.move(115, 48)
-  screen.text_right(params:get("amp" .. current_page))
+  screen.text_right(params:get("amp" .. current_res))
   screen.move(15, 60)
   screen.text("ring: ")
   screen.move(115, 60)
-  screen.text_right(params:get("ring" .. current_page))
+  screen.text_right(params:get("ring" .. current_res))
 end
 
 
@@ -220,10 +273,10 @@ function midi_event(data)
     --print("yes")
     --note_on(msg.note, msg.vel / 127)
     if midi_tune == true then
-      params:set("freq" .. current_page, MusicUtil.note_num_to_freq(msg.note))
+      params:set("freq" .. current_res, MU.note_num_to_freq(msg.note))
     else
       print("hi")
-      engine.offset(MusicUtil.note_num_to_freq(msg.note))
+      engine.offset(MU.note_num_to_freq(msg.note))
     end
   end
 end
